41b76f16ba69221b7efc2e3d3f1ce47915e0e7fd
Michael Pickerill <zorzac2001@gmail.com>
2020-06-27 15:53:58 -0500

    Added a JavaScript file.

---
+++ function.js
@@ -0,0 +1,7 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: blue; icon-glyph: magic;
+function creator () {eat =["caffeine", "coffee"];
+console.log(eat);}
+creator();
+Script.complete()
