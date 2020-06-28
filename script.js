cbd0b2cfde5986bb5548f4da9edada955033bdf6
Michael Pickerill <zorzac2001@gmail.com>
2020-06-27 21:02:10 -0500

    added another JavaScript file

---
+++ script.js
@@ -0,0 +1,8 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-blue; icon-glyph: magic;
+let eat = 111;
+console.log(eat);
+console.log("I have"+eat+" "+"apples.");
+const py = 3.14; 
+console.log(py);
